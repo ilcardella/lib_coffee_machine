b5dcb645a3ac17857b7275fab3a6661de3b652bb
Alberto Cardellini <albe.carde@gmail.com>
2021-02-14 19:11:48 +0000

    corrected typo

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 # Build the automated test suite
 mkdir -p build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr/local/lib --DBUILD_TESTS=OFF ..
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr/local/lib -DBUILD_TESTS=OFF ..
 cmake --build . --config Release --target install -- -j $(nproc)
