a5db3022df1294b4e93d3a1d923adb2534d5c26a
Alberto Cardellini <albe.carde@gmail.com>
2021-02-14 22:02:09 +0000

    corrected typo

--- scripts/test-project.sh
+++ scripts/test-project.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # Build the automated test suite
 mkdir -p build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=/tmp/lib_coffee_machine --DBUILD_TESTS=ON ..
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/tmp/lib_coffee_machine -DBUILD_TESTS=ON ..
 cmake --build . --config Release -- -j $(nproc)
 # Test installation target
 cmake --build . --config Release --target install -- -j $(nproc)
