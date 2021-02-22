a15476bc4bd403d381eb0c62eeeea57430ba72ef
Alberto Cardellini <albe.carde@gmail.com>
2021-02-22 23:20:09 +0000

    Simplified Adapter class in the test and example (#5)

--- tests/mock/test_adapter.h
+++ tests/mock/test_adapter.h
@@ -6,21 +6,6 @@
 class TestAdapter
 {
   public:
-    inline static void pinMode(uint8_t pin, uint8_t mode)
-    {
-        ;
-    }
-
-    inline static int digitalRead(uint8_t pin)
-    {
-        return 0;
-    }
-
-    inline static void digitalWrite(uint8_t pin, uint8_t val)
-    {
-        ;
-    }
-
     inline static void delay(unsigned long ms)
     {
         sleep(ms / 1000);
@@ -32,37 +17,6 @@ class TestAdapter
         // return duration_cast<milliseconds>(system_clock::now().time_since_epoch())
         //     .count();
         return millis_ret;
-    }
-
-    inline static void SerialBegin(const unsigned long &baudrate)
-    {
-        ;
-    }
-
-    inline static int SerialAvailable()
-    {
-        return 1;
-    }
-
-    inline static void SerialReadStringUntil(const char &terminator, char *data)
-    {
-        ;
-    }
-
-    inline static size_t SerialPrintln(const char *value)
-    {
-        strncpy(message_sent, value, 100);
-        return 0;
-    }
-
-    inline static void WireBegin()
-    {
-        ;
-    }
-
-    inline static void WireSetClock(const uint32_t &clock)
-    {
-        ;
     }
 
     inline static char *dtostrf(double val, signed char width, unsigned char prec,
